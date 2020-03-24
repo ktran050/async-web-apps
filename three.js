a561cd562102844f8f9405929652c3bf8129d7be
Kevin Tran <ktran050@gmail.com>
2020-03-23 21:57:39 -0700

    sucessfully show a picture based on a breed if its found

--- three.js
+++ three.js
@@ -1,9 +1,24 @@
 function handleSubmit() {
   $("#form2").on("click", "#submit2", function(event) {
     event.preventDefault();
-    console.log("breed value", $("#breedInput").val());
+    let userInput = $("#breedInput").val();
+    fetch(`https://dog.ceo/api/breed/${userInput}/images/random`)
+      .then(function(result) {
+        if (!result.ok) {
+          throw new Error("result not ok");
+        } else {
+          return result.json();
+        }
+      })
+      .then(function(result) {
+        $("#container2").html(`<img src="${result.message}"></img>`);
+      })
+      .catch(function(error) {
+        console.log("error", error);
+      });
   });
 }
+
 function handleThree() {
   console.log("three handled");
   handleSubmit();
